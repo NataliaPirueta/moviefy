861a0ecba6962090fc3d3cb178e3e3758b163e4f
Natalia <birruetanatalia@gmail.com>
2025-05-31 15:53:59 -0700

    Agregar función renderMovies para mostrar películas al cargar el sitio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { API_CONFIG } from "./config"
+import { API_CONFIG } from "./config.js";
 
 //Peticiones
 const API_HEADERS = {
@@ -11,23 +11,53 @@ const moviesContainer = document.querySelector('.movies')
 const searchInput = document.querySelector('#search')
 const searchButton = document.querySelector('#search-btn')
 
-function getNowPlayingMovies(){
+const getNowPlayingMovies = async() => {
 
-    try{
-        const response = await fetch(`${API_CONFIG.baseUrl}/movie/now_playing?language=es-MX&page=1`,{
+    try {
+        const response = await fetch(`${API_CONFIG.baseUrl}/movie/now_playing?language=es-MX&page=1`, {
             headers: API_HEADERS
         });
-
-        if(!response.ok) {
+        
+        if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
+        
         const data = await response.json();
         return data.results;
 
-    } catch(error) {
-        console.error('Error fetching movies: ', error);
-        return[];
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        return [];
     }
 
 };
+
+const renderMovies = (movies) => {
+
+    if(!moviesContainer) return;
+
+    moviesContainer.innerHTML = movies.map(movie=> `
+        
+       <div class=movie-card">
+
+            <img src="${API_CONFIG.imgUrl}${movie.poster_path}"
+            alt="${movie.title}"
+            onerror="this.src'https://via.placeholder.com/500x750?text=No+Image'">
+            <h3>${movie.title}</h3>
+
+       </div> 
+        
+    `).join('');
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Verificamos que el token esté configurado
+    if (!API_CONFIG.token) {
+        console.error('API Token no configurado. Revisa tu archivo .env');
+        return;
+    }
+    
+    // Cargar las películas del momento
+    const movies = await getNowPlayingMovies();
+    renderMovies(movies);
+});
