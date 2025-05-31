466812affdf5b1a935cd86837c6885be4101837e
Natalia <birruetanatalia@gmail.com>
2025-05-31 15:24:00 -0700

    Configuración de la API e implementación de función para obtener las películas del momento

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-import { API_TOKEN, API_BASE_URL } from './config.js';
+import { API_CONFIG } from "./config"
 
-//Elementos nodo
-const form = document.querySelector('form')
+//Peticiones
+const API_HEADERS = {
+    'Authorization': `Bearer ${API_CONFIG.token}`,
+    'Content-Type': 'application/json'
+};
+
+//Selectores DOM
 const moviesContainer = document.querySelector('.movies')
+const searchInput = document.querySelector('#search')
+const searchButton = document.querySelector('#search-btn')
 
+function getNowPlayingMovies(){
+
+    try{
+        const response = await fetch(`${API_CONFIG.baseUrl}/movie/now_playing?language=es-MX&page=1`,{
+            headers: API_HEADERS
+        });
+
+        if(!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.results;
+
+    } catch(error) {
+        console.error('Error fetching movies: ', error);
+        return[];
+    }
+
+};
