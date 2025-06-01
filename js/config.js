a9dea30fec8df5b0ad153e9c78abf7801ed8d3d8
Natalia <birruetanatalia@gmail.com>
2025-06-01 14:21:32 -0700

    Cambios en .gitignore

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const API_CONFIG ={
+    token:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDVkYTViMTRjNzRjMjI4OTdlN2VhMTM2MjJlMDFhYSIsIm5iZiI6MTczMjEzMjcwMS40NDEsInN1YiI6IjY3M2UzZjVkNGRlYzFmOThiMjliY2VjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fFDeh2Uv-xGrKxnisR9KtTHq2JhXArkOVQKjA1VuURo',
+    baseUrl: 'https://api.themoviedb.org/3',
+    imgUrl: 'https://image.tmdb.org/t/p/w500/'
+};
